32033609b2668c56d4147f38598794b7b2b3aafe
akhmed-jjb <akhmed.95r@mail.ru>
2025-02-02 20:26:35 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+body {
+    font-family: Arial, sans-serif;
+    text-align: center;
+}
+header {
+    background-color: #222;
+    color: white;
+    padding: 20px;
+}
+footer {
+    margin-top: 20px;
+    font-size: 14px;
+}
